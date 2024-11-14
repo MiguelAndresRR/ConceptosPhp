950c23dd01cbba232030f9e1277f75d388192651
macro <macrolive578@gmail.com>
2024-11-14 13:51:53 -0500

    first commit

---
+++ php/comentario2.php
@@ -0,0 +1,13 @@
+<html>
+    <link rel="stylesheet" href="php/.css">
+    <body>
+        <?php
+        /*
+        este es comentario abarca varias líneas.
+        Los comentarios son útiles para documentar los
+        programas.
+        */
+        echo "hola mundo" ;
+        ?>
+    </body>
+</html>
