b570a41539f6c6dddf54438dea9fc5fca8f1a80a
macro <macrolive578@gmail.com>
2024-11-14 16:24:21 -0500

    first commit

---
+++ php/ciclosdowhile1.php
@@ -0,0 +1,9 @@
+<?php
+    echo"Codigo PHP para generar numero del 1 al 5 "."<br>";
+    $i = 1;
+    do {
+        echo "<br>el valor de i es ".$i."<br>";
+        $i++;
+    }
+    while($i <= 5);
+?>
---
+++ php/cicloswhile1.php
@@ -0,0 +1,8 @@
+<?php
+    echo"Codigo PHP para generar numeros del 1 al 10";
+    $i = 1;
+    while($i < 10){
+        echo "el valor de i es ".$i."<br>";
+        $i++;
+    };
+?>
