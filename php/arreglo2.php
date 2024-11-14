dd5f913b2e0a02e4f014dd2b0ea77acc9ec51b8c
macro <macrolive578@gmail.com>
2024-11-14 15:12:10 -0500

    first commit

---
+++ php/arreglo2.php
@@ -0,0 +1,9 @@
+<?php
+    $arreglo = [10000,"Jose","Pepita","Mendieta"] ;
+    echo "Buenos días ". $arreglo[0] ." ". $arreglo[1]." ".
+    $arreglo[2] ." ".$arreglo[3] . "<br>";
+    var_dump($arreglo);
+    // proporciona información sobre el tamaño y tipo de datos de la variable y, en el caso de arrays y 
+    //objetos, de los elementos que la componen.
+    !$arreglo
+?>
